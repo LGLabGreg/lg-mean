a9a407380c43fd7825123d76fc1ddf0a22aa06a0
Gregory Lebret <greg@lglab.co.uk>
2016-04-28 16:45:21 +0100

    added put route (not tested)

--- app/routes.js
+++ app/routes.js
@@ -42,6 +42,34 @@ module.exports = function(app) {
   });
 
   // delete a todo
+  app.put('/api/todos/:todo_id', function(req, res) {
+    Todo.findById({
+      _id: req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // update todo
+      todo.text = req.body.text;
+      todo.done = req.body.done;
+      // save the todo
+      todo.save(function(err) {
+        if (err)
+          res.send(err);
+        console.log('User successfully updated!');
+      });
+
+      // get and return all the todos after you create another
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+      
+    });
+  });
+
+  // delete a todo
   app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
       _id: req.params.todo_id
